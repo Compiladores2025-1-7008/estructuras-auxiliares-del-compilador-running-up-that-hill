050ef3b79d694cf59b1cce533be9a637027951e8
NormaSeleneSanchez <123840003+NormaSeleneSanchez@users.noreply.github.com>
2025-12-07 18:58:56 -0600

    Add class documentation for CodeGenerator
    Added documentation for the CodeGenerator class.

--- src/CodeGenerator.hpp
+++ src/CodeGenerator.hpp
@@ -3,10 +3,16 @@
 
 #include <string>
 
+/**
+ * @class CodeGenerator
+ * Clase responsable de generar codigo intermedio de tres direcciones
+ * Proporciona funcionalidades para generar codigo intermedio, incluyendo la creacion de variables temporales y etiquetas 
+ * unicas. Se utiliza en la fase de generación de codigo de un compilador, despues del analisis sintactico y semantico.
+ */
 class CodeGenerator {
 private:
-    int tempCount;
-    int labelCount;
+    int tempCount; //  Contador para generar nombres unicos de variables temporales 
+    int labelCount; // Contador para generar nombres unicos de etiquetas
 
 public:
     CodeGenerator();
