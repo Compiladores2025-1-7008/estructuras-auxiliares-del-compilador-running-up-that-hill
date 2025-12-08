2d9c8f68a88b0495305e35b75c2220cb4d8160b1
NormaSeleneSanchez <123840003+NormaSeleneSanchez@users.noreply.github.com>
2025-12-07 18:51:03 -0600

    Enhance comments in CodeGenerator methods
    Updated comments to provide more detailed explanations for newTemp and newLabel methods.

--- src/CodeGenerator.cpp
+++ src/CodeGenerator.cpp
@@ -9,13 +9,20 @@ CodeGenerator::CodeGenerator() : tempCount(0), labelCount(0) {}
 
 /**
  * Genera un nuevo ID para una variable temporal 
- * Crea una nueva variable temporal con el formato tn donde n es un numero secuencial que se incrementa con cada llamada.
+ * Crea una nueva variable temporal con el formato tn donde n es un numero secuencial que se incrementa con cada llamada, 
+ * que se utilizan para almacenar resultados intermedios durante la evaluación de expresiones.
  * @return std::string ID de la variable temporal
  */
 std::string CodeGenerator::newTemp() {
     return "t" + std::to_string(tempCount++);
 }
 
+/**
+ * Genera un nuevo ID para una etiqueta
+ * Crea una nueva etiqueta con el formato "Ln" donde n es un numero secuencial que se incrementa con cada llamada, que se
+ * utilizan para marcar posiciones en el código intermedio, para implementar estructuras de control como bucles y condicionales.
+ * @return std::string ID de la etiqueta
+ */
 std::string CodeGenerator::newLabel() {
     return "L" + std::to_string(labelCount++);
 }
