9b8dfedd4bb1e16d512bfe4ae9a2a1b168e4c1a4
NormaSeleneSanchez <123840003+NormaSeleneSanchez@users.noreply.github.com>
2025-12-07 18:35:19 -0600

    Implement CodeGenerator constructor with documentation
    Added a constructor with documentation for CodeGenerator class.

--- src/CodeGenerator.cpp
+++ src/CodeGenerator.cpp
@@ -1,6 +1,13 @@
 #include "CodeGenerator.hpp"
 
+/**
+ * @brief Constructor de la clase CodeGenerator
+ * 
+ * Inicializa los contadores de variables temporales y etiquetas en cero. estos contadores se utilizan 
+ * para generar nombres unicos durante la generacion de codigo intermedio.
+ */
 CodeGenerator::CodeGenerator() : tempCount(0), labelCount(0) {}
+
 
 std::string CodeGenerator::newTemp() {
     return "t" + std::to_string(tempCount++);
