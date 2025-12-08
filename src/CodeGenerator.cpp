ecb2864bf5b735dca6299575e72e7a5b21e68432
NormaSeleneSanchez <123840003+NormaSeleneSanchez@users.noreply.github.com>
2025-12-07 18:41:12 -0600

    Improve documentation for CodeGenerator class
    Updated constructor documentation and added newTemp method documentation.

--- src/CodeGenerator.cpp
+++ src/CodeGenerator.cpp
@@ -1,14 +1,17 @@
 #include "CodeGenerator.hpp"
 
 /**
- * @brief Constructor de la clase CodeGenerator
- * 
- * Inicializa los contadores de variables temporales y etiquetas en cero. estos contadores se utilizan 
- * para generar nombres unicos durante la generacion de codigo intermedio.
+ *Constructor de la clase CodeGenerator 
+ * Inicializa los contadores de variables temporales y etiquetas en cero, estos contadores se utilizan 
+ * para generar nombres unicos (ID'S) durante la generacion de codigo intermedio.
  */
 CodeGenerator::CodeGenerator() : tempCount(0), labelCount(0) {}
 
-
+/**
+ * Genera un nuevo ID para una variable temporal 
+ * Crea una nueva variable temporal con el formato tn donde n es un numero secuencial que se incrementa con cada llamada.
+ * @return std::string ID de la variable temporal
+ */
 std::string CodeGenerator::newTemp() {
     return "t" + std::to_string(tempCount++);
 }
